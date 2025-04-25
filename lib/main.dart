63aed09f0be2f0157259f0401bd1ca2cd8804146
AthiraaAthi <athiraa2612@gmail.com>
2025-04-25 19:06:15 +0530

    added debugShowCheckedModeBanner flag to MaterialApp in MeditationApp

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MeditationApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
