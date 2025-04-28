ce2ba189a5c5c73312955afae4da316b2e8dfc8d
AthiraaAthi <athiraa2612@gmail.com>
2025-04-28 16:30:46 +0530

    Added BottomWaveClipper and updated ClipPath in StartingScreen for enhanced visual design

--- lib/view/starting_page/starting.dart
+++ lib/view/starting_page/starting.dart
@@ -65,7 +65,13 @@ class StartingScreen extends StatelessWidget {
                 ),
                 Align(
                   alignment: Alignment.bottomCenter,
-                  child: ClipPath(),
+                  child: ClipPath(
+                    clipper: BottomWaveClipper(),
+                    child: Container(
+                      height: 80,
+                      color: Colors.white,
+                    ),
+                  ),
                 )
               ]),
             ),
@@ -74,4 +80,39 @@ class StartingScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class BottomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height - 30);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2, size.height - 30);
+
+    var secondControlPoint = Offset(3 * size.width / 4, size.height - 60);
+    var secondEndPoint = Offset(size.width, size.height - 30);
+
+    path.quadraticBezierTo(
+      firstControlPoint.dx,
+      firstControlPoint.dy,
+      firstEndPoint.dx,
+      firstEndPoint.dy,
+    );
+    path.quadraticBezierTo(
+      secondControlPoint.dx,
+      secondControlPoint.dy,
+      secondEndPoint.dx,
+      secondEndPoint.dy,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
