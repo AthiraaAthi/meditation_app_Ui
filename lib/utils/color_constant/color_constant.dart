79b6e26264417accf11beb51f548e50bc64debe0
AthiraaAthi <athiraa2612@gmail.com>
2025-04-30 15:49:21 +0530

    Added txtBlueGrey color constant for consistent theming across the app

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color white = Colors.white;
 const Color black = Colors.black;
 const Color defBlue = Color(0xFF8E97FD);
+const Color txtBlueGrey = Color(0xFFA1A4B2);
