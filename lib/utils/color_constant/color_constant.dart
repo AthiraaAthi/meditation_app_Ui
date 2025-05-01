f2dadd374f0d0033dd5f2b4e6b1f0a5b6418be06
AthiraaAthi <athiraa2612@gmail.com>
2025-05-01 17:23:26 +0530

    Added new color  constant for consistent theming

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -4,3 +4,4 @@ const Color white = Colors.white;
 const Color black = Colors.black;
 const Color defBlue = Color(0xFF8E97FD);
 const Color txtBlueGrey = Color(0xFFA1A4B2);
+const Color purple = Color(0xFF17583CA);
