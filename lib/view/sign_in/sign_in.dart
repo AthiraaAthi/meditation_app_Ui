16693be3ee7d3328ea809bdfe99badf654a534aa
AthiraaAthi <athiraa2612@gmail.com>
2025-05-01 17:21:56 +0530

    Set fontWeight for welcome message in SignInScreen for improved emphasis

--- lib/view/sign_in/sign_in.dart
+++ lib/view/sign_in/sign_in.dart
@@ -15,6 +15,7 @@ class SignInScreen extends StatelessWidget {
             Text("Welcome Back!",
                 style: TextStyle(
                   fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 )),
           ],
         ),
