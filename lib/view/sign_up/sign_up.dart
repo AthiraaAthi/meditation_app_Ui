34e940afcea16e88c464a6679c2226f2d56173de
AthiraaAthi <athiraa2612@gmail.com>
2025-05-13 16:29:16 +0530

    Added border to password TextField in SignUpScreen for improved styling.

--- lib/view/sign_up/sign_up.dart
+++ lib/view/sign_up/sign_up.dart
@@ -135,6 +135,7 @@ class SignUpScreen extends StatelessWidget {
                   fillColor: textfieldFill,
                   contentPadding:
                       EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                  border: OutlineInputBorder(),
                 ),
               ),
             ],
