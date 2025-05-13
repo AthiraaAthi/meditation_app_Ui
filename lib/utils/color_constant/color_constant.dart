7c69b17e9a13ed243c0b21f554fce37633e0690d
AthiraaAthi <athiraa2612@gmail.com>
2025-05-13 16:24:29 +0530

    Added welcomeBlue color constant for improved color management in the app.

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -6,3 +6,4 @@ const Color defBlue = Color(0xFF8E97FD);
 const Color txtBlueGrey = Color(0xFFA1A4B2);
 const Color purple = Color(0xFF17583CA);
 const Color textfieldFill = Color(0xF5F6F9FF);
+const Color welcomeBlue = Color(0xFF8C96FF);
