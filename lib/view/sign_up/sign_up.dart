00f51ee262dcf6214001563f7043174554913d18
AthiraaAthi <athiraa2612@gmail.com>
2025-05-13 16:34:16 +0530

    Refactored SignUpScreen layout for improved structure and readability

--- lib/view/sign_up/sign_up.dart
+++ lib/view/sign_up/sign_up.dart
@@ -12,143 +12,145 @@ class SignUpScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("Create Your Account",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                height: 50,
-                width: 375,
-                decoration: BoxDecoration(
-                  color: purple,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(image: AssetImage(fbIcon))),
-                    ),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Text("CONTINUE WITH FACEBOOK",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: white,
-                        )),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                height: 50,
-                width: 375,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.grey.shade200, width: 2),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          image:
-                              DecorationImage(image: AssetImage(googleIcon))),
-                    ),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Text("CONTINUE WITH GOOGLE",
-                        style: TextStyle(fontSize: 16, color: black)),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "OR LOG IN WITH EMAIL",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Create Your Account",
                     style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold),
-                  )),
-              SizedBox(
-                height: 25,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: textfieldFill,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 50,
+                  width: 375,
+                  decoration: BoxDecoration(
+                    color: purple,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(image: AssetImage(fbIcon))),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text("CONTINUE WITH FACEBOOK",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: white,
+                          )),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Email address',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: textfieldFill,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 50,
+                  width: 375,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.grey.shade200, width: 2),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            image:
+                                DecorationImage(image: AssetImage(googleIcon))),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text("CONTINUE WITH GOOGLE",
+                          style: TextStyle(fontSize: 16, color: black)),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  filled: true,
-                  fillColor: textfieldFill,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.visibility_off,
-                    color: txtBlueGrey,
+                SizedBox(
+                  height: 25,
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "OR LOG IN WITH EMAIL",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold),
+                    )),
+                SizedBox(
+                  height: 25,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: textfieldFill,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email address',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: textfieldFill,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: textfieldFill,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.visibility_off,
+                      color: txtBlueGrey,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
