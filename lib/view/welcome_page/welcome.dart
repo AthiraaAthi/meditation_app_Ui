46d4f646bb7058074f9199bfe0826ac7e7a1b08c
AthiraaAthi <athiraa2612@gmail.com>
2025-05-15 17:34:56 +0530

    Added background color to WelcomeScreen and organized imports for clarity

--- lib/view/welcome_page/welcome.dart
+++ lib/view/welcome_page/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app_ui/utils/color_constant/color_constant.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -6,6 +7,7 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: welcomeBg,
       body: SafeArea(
         child: Column(
           children: [
@@ -15,10 +17,10 @@ class WelcomeScreen extends StatelessWidget {
                 const Text(
                   "Silent",
                   style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 4,
-                  ),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 4,
+                      color: white),
                 ),
               ],
             ),
