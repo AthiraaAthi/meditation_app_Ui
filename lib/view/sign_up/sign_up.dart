b0b928102f0216bb260760b41c3fb1980c188ff0
AthiraaAthi <athiraa2612@gmail.com>
2025-05-15 17:26:45 +0530

    Added onTap functionality to InkWell in SignUpScreen for improved interactivity

--- lib/view/sign_up/sign_up.dart
+++ lib/view/sign_up/sign_up.dart
@@ -196,6 +196,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   height: 25,
                 ),
                 InkWell(
+                  onTap: () {},
                   child: Container(
                     height: 50,
                     width: 350,
