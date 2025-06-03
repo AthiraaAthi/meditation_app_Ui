bcdd5d061fe57e03f00c4f8906c919e8bb35da88
AthiraaAthi <athiraa2612@gmail.com>
2025-06-03 16:22:33 +0530

    Added Center widget to the button in WelcomeScreen for improved alignment

--- lib/view/welcome_page/welcome.dart
+++ lib/view/welcome_page/welcome.dart
@@ -96,6 +96,7 @@ class WelcomeScreen extends StatelessWidget {
                   color: Color(0xffEBEAEC),
                   borderRadius: BorderRadius.circular(30),
                 ),
+                child: Center(),
               ),
             ],
           ),
