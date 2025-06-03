2a20982af08f4c8cb2586fa0cf3fb56c22adb20f
AthiraaAthi <athiraa2612@gmail.com>
2025-06-03 16:33:35 +0530

    Added TopicSelectionScreen widget with basic structure

--- lib/view/choose_topic/topic_screen.dart
+++ lib/view/choose_topic/topic_screen.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class TopicSelectionScreen extends StatelessWidget {
+  const TopicSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
