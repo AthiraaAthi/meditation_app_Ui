7e0524da2fb531dddddfdc88c59194c6ebe4cf73
AthiraaAthi <athiraa2612@gmail.com>
2025-06-05 17:17:59 +0530

    Added new image asset for better sleep

--- lib/utils/image_constant/image_constant.dart
+++ lib/utils/image_constant/image_constant.dart
@@ -3,3 +3,4 @@ const String blueMoonIcon = "assets/images/silentmoon.png";
 const String googleIcon = "assets/images/google.png";
 const String fbIcon = "assets/images/fb.png";
 const String welcomeImage = "assets/images/welcome_img.png";
+const String better_Sleep = "assets/images/better_sleep.png";
