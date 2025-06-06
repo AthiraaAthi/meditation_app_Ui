bfcf354816b3c6a15fd68ef6a1c1ffb2121cdd83
AthiraaAthi <athiraa2612@gmail.com>
2025-06-06 17:12:04 +0530

    Added image asset for performance improvement

--- lib/utils/image_constant/image_constant.dart
+++ lib/utils/image_constant/image_constant.dart
@@ -4,3 +4,4 @@ const String googleIcon = "assets/images/google.png";
 const String fbIcon = "assets/images/fb.png";
 const String welcomeImage = "assets/images/welcome_img.png";
 const String betterSleep = "assets/images/better_sleep.png";
+const String improveperfo = "assets/images/imprv_perfo.png";
