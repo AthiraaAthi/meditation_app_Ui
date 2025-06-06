39244ca95d8db7fd4fd124af38a9b371912f6c00
AthiraaAthi <athiraa2612@gmail.com>
2025-06-06 17:12:36 +0530

    Added image asset for increasing happiness

--- lib/utils/image_constant/image_constant.dart
+++ lib/utils/image_constant/image_constant.dart
@@ -5,3 +5,4 @@ const String fbIcon = "assets/images/fb.png";
 const String welcomeImage = "assets/images/welcome_img.png";
 const String betterSleep = "assets/images/better_sleep.png";
 const String improveperfo = "assets/images/imprv_perfo.png";
+const String increaseHappiness = "assets/images/incrse_hppiness.png";
