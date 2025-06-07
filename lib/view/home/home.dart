0888c65bd3283ac5f0b31e4519406b0782b4ee0d
AthiraaAthi <athiraa2612@gmail.com>
2025-06-07 17:59:12 +0530

    Set mainAxisAlignment to center in Row widget of HomeScreen

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -7,7 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
     );
   }
 }
