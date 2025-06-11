17a91be2ac3deaddfda3eed5bd47dea2c74ad84d
AthiraaAthi <athiraa2612@gmail.com>
2025-06-11 17:22:45 +0530

    Refactored NavScreen to use StatefulWidget for enhanced state management

--- lib/view/nav_screen/nav_screen.dart
+++ lib/view/nav_screen/nav_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class NavScreen extends StatelessWidget {
+class NavScreen extends StatefulWidget {
   const NavScreen({super.key});
 
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
