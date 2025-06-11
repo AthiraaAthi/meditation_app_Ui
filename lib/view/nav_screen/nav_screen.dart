2512bc61d84e68868dca34b4b4b3d8eeb3724410
AthiraaAthi <athiraa2612@gmail.com>
2025-06-11 17:30:11 +0530

    Added navigation for SleepScreen, MedidateScreen, MusicScreen, and ProfileScreen in NavScreen

--- lib/view/nav_screen/nav_screen.dart
+++ lib/view/nav_screen/nav_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meditation_app_ui/view/home/home.dart';
+import 'package:meditation_app_ui/view/meditate_screen/medidate_screen.dart';
+import 'package:meditation_app_ui/view/music_screen/music_screen.dart';
+import 'package:meditation_app_ui/view/profile_screen/profile.dart';
+import 'package:meditation_app_ui/view/sleep_screen/sleep_screen.dart';
 
 class NavScreen extends StatefulWidget {
   const NavScreen({super.key});
@@ -11,6 +15,10 @@ class NavScreen extends StatefulWidget {
 class _NavScreenState extends State<NavScreen> {
   final List<Widget> screens = [
     HomeScreen(),
+    SleepScreen(),
+    MedidateScreen(),
+    MusicScreen(),
+    ProfileScreen()
   ];
   @override
   Widget build(BuildContext context) {
