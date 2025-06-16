6331d0dde31c562f1cd26e4f8a6b3bf9be9d4977
AthiraaAthi <athiraa2612@gmail.com>
2025-06-16 10:54:42 +0530

    Added happiness image to the images list in HomeScreen state

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<String> images = [
     focus,
+    happiness,
   ];
   @override
   Widget build(BuildContext context) {
