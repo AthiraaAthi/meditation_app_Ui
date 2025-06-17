107e758cb112c042fd0f5cbf759ac4b52136b55c
AthiraaAthi <athiraa2612@gmail.com>
2025-06-17 17:59:33 +0530

    Refactored CourseScreen layout to wrap body in SafeArea for improved UI safety

--- lib/view/home/course_screen.dart
+++ lib/view/home/course_screen.dart
@@ -12,33 +12,35 @@ class _CourseScreenState extends State<CourseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              decoration:
-                  BoxDecoration(image: DecorationImage(image: AssetImage(sun))),
-            ),
-            Positioned(
-              top: 40,
-              left: 20,
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.arrow_back),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(sun))),
               ),
-            ),
-            Positioned(
-              top: 40,
-              right: 80,
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.favorite_border),
+              Positioned(
+                top: 40,
+                left: 20,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.arrow_back),
+                ),
               ),
-            ),
-          ])
-        ],
+              Positioned(
+                top: 40,
+                right: 80,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.favorite_border),
+                ),
+              ),
+            ])
+          ],
+        ),
       ),
     );
   }
