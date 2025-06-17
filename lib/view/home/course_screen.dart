de499ff96445cf4e10f6e587ad03cf8d8713e541
AthiraaAthi <athiraa2612@gmail.com>
2025-06-17 17:49:58 +0530

    Refactored CourseScreen to use StatefulWidget and implement initial layout

--- lib/view/home/course_screen.dart
+++ lib/view/home/course_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class CourseScreen extends StatelessWidget {
+class CourseScreen extends StatefulWidget {
   const CourseScreen({super.key});
 
   @override
+  State<CourseScreen> createState() => _CourseScreenState();
+}
+
+class _CourseScreenState extends State<CourseScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
