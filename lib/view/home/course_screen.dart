aa51fa10f5c8d0cf8196ac115065e426d2b73224
AthiraaAthi <athiraa2612@gmail.com>
2025-06-18 16:57:19 +0530

    Added empty Text widget to CourseScreen for layout consistency

--- lib/view/home/course_screen.dart
+++ lib/view/home/course_screen.dart
@@ -48,6 +48,7 @@ class _CourseScreenState extends State<CourseScreen> {
               ),
             ]),
             SizedBox(height: 20),
+            Text("")
           ],
         ),
       ),
