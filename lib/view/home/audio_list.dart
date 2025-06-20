e59066f6929e3943b3c8767379ef1fd1d83bd276
AthiraaAthi <athiraa2612@gmail.com>
2025-06-20 18:09:18 +0530

    Added AudioTile widget for audio list representation

--- lib/view/home/audio_list.dart
+++ lib/view/home/audio_list.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class AudioTile extends StatelessWidget {
+  final String title;
+  final String duration;
+  final bool isPlaying;
+
+  const AudioTile({
+    required this.title,
+    required this.duration,
+    this.isPlaying = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile();
+  }
+}
