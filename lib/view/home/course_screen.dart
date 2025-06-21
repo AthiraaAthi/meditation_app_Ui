b65199f1ffe0f1eeae43dc8e85746893bc1a5a57
AthiraaAthi <athiraa2612@gmail.com>
2025-06-21 17:32:10 +0530

    Refactored CourseScreen layout for improved structure and added scrolling capability

--- lib/view/home/course_screen.dart
+++ lib/view/home/course_screen.dart
@@ -15,137 +15,139 @@ class _CourseScreenState extends State<CourseScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage(sun))),
-              ),
-              Positioned(
-                top: 40,
-                left: 20,
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.arrow_back),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(sun))),
                 ),
-              ),
-              Positioned(
-                top: 40,
-                right: 80,
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.favorite_border),
+                Positioned(
+                  top: 40,
+                  left: 20,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.arrow_back),
+                  ),
                 ),
-              ),
-              Positioned(
-                top: 40,
-                right: 20,
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.download_outlined),
+                Positioned(
+                  top: 40,
+                  right: 80,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.favorite_border),
+                  ),
                 ),
-              ),
-            ]),
-            //SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Happy Morning",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Positioned(
+                  top: 40,
+                  right: 20,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.download_outlined),
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    "COURSE",
-                    style: TextStyle(
-                        letterSpacing: 2, color: txtgrey, fontSize: 16),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Ease the mind into a restful night's sleep with these deep, ambient tones.",
-                    style: TextStyle(
-                      color: txtgrey,
-                      fontSize: 17,
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Icon(Icons.favorite, color: Colors.pink, size: 20),
-                      SizedBox(width: 5),
-                      Text(
-                        "24,234 Favorites",
-                        style: TextStyle(
-                          color: txtgrey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
+                ),
+              ]),
+              //SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Happy Morning",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                      SizedBox(width: 20),
-                      Icon(
-                        Icons.headphones,
-                        color: Colors.lightBlue,
-                        size: 20,
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        "34,234 Listening",
-                        style: TextStyle(
-                          color: txtgrey,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Pick a Narrator',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  DefaultTabController(
-                      length: 2,
-                      child: Column(
-                        children: [
-                          TabBar(
-                            indicatorColor: defBlue,
-                            labelColor: defBlue,
-                            unselectedLabelColor: Colors.grey,
-                            tabs: [
-                              Tab(text: 'MALE VOICE'),
-                              Tab(text: 'FEMALE VOICE'),
-                            ],
+                    SizedBox(height: 5),
+                    Text(
+                      "COURSE",
+                      style: TextStyle(
+                          letterSpacing: 2, color: txtgrey, fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Ease the mind into a restful night's sleep with these deep, ambient tones.",
+                      style: TextStyle(
+                        color: txtgrey,
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Icon(Icons.favorite, color: Colors.pink, size: 20),
+                        SizedBox(width: 5),
+                        Text(
+                          "24,234 Favorites",
+                          style: TextStyle(
+                            color: txtgrey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(
-                            height: 100,
-                            child: TabBarView(
-                              children: [
-                                AudioList(),
-                                AudioList(),
+                        ),
+                        SizedBox(width: 20),
+                        Icon(
+                          Icons.headphones,
+                          color: Colors.lightBlue,
+                          size: 20,
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "34,234 Listening",
+                          style: TextStyle(
+                            color: txtgrey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Pick a Narrator',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    DefaultTabController(
+                        length: 2,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              indicatorColor: defBlue,
+                              labelColor: defBlue,
+                              unselectedLabelColor: Colors.grey,
+                              tabs: [
+                                Tab(text: 'MALE VOICE'),
+                                Tab(text: 'FEMALE VOICE'),
                               ],
                             ),
-                          ),
-                        ],
-                      ))
-                ],
-              ),
-            )
-          ],
+                            SizedBox(
+                              height: 100,
+                              child: TabBarView(
+                                children: [
+                                  AudioList(),
+                                  AudioList(),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
