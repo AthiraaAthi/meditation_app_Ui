d8336f82640c64ef050eeb7f9f509d3e8b45dd96
AthiraaAthi <athiraa2612@gmail.com>
2025-06-21 17:19:25 +0530

    Added padding to AudioList for improved layout consistency

--- lib/view/home/audio_list.dart
+++ lib/view/home/audio_list.dart
@@ -35,6 +35,7 @@ class AudioList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       physics: NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.all(16),
     );
   }
 }
