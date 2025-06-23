03cd5100c0c1c0204333e92a0182b5beb3ba960d
AthiraaAthi <athiraa2612@gmail.com>
2025-06-23 17:33:21 +0530

    Added spacing and a ListView to MedidateScreen for improved layout and organization

--- lib/view/meditate_screen/medidate_screen.dart
+++ lib/view/meditate_screen/medidate_screen.dart
@@ -26,7 +26,9 @@ class MedidateScreen extends StatelessWidget {
               Text(
                 "we can learn how to recognize when our minds\n are doing their normal everyday acrobatics.",
                 style: TextStyle(fontSize: 13, color: txtgrey),
-              )
+              ),
+              SizedBox(height: 20),
+              ListView()
             ],
           ),
         ),
