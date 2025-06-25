2d5263c53b92b61129b98c79dc271b3dfb754fb8
AthiraaAthi <athiraa2612@gmail.com>
2025-06-25 15:54:39 +0530

    Refactored MedidateScreen layout for improved structure and added horizontal scrolling to options ListView

--- lib/view/meditate_screen/medidate_screen.dart
+++ lib/view/meditate_screen/medidate_screen.dart
@@ -27,83 +27,88 @@ class _MedidateScreenState extends State<MedidateScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Meditate',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Meditate',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Text(
+                  "we can learn how to recognize when our minds\n are doing their normal everyday acrobatics.",
+                  style: TextStyle(fontSize: 16, color: txtgrey),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 95,
+                  child: ListView.builder(
+                    itemCount: options.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Container(
+                        width: 65,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(options[index]),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
                   ),
-                ],
-              ),
-              Text(
-                "we can learn how to recognize when our minds\n are doing their normal everyday acrobatics.",
-                style: TextStyle(fontSize: 16, color: txtgrey),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                height: 95,
-                child: ListView.builder(
-                  itemCount: options.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: Container(
-                      width: 65,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 95,
+                  width: 375,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(dailyCalm),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: grid.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 10,
+                        mainAxisExtent: 200),
+                    itemBuilder: (context, index) => Container(
+                      width: 170,
+                      height: 210,
                       decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(options[index]),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.circular(10)),
+                        image: DecorationImage(
+                            image: AssetImage(grid[index]), fit: BoxFit.cover),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 95,
-                width: 375,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(dailyCalm),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: GridView.builder(
-                  itemCount: grid.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 10,
-                      mainAxisExtent: 200),
-                  itemBuilder: (context, index) => Container(
-                    width: 170,
-                    height: 210,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(grid[index]), fit: BoxFit.cover),
-                    ),
-                  ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
