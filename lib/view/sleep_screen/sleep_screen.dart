3ff85d173aa58d0d177061a394b763d7c065545c
AthiraaAthi <athiraa2612@gmail.com>
2025-06-25 16:15:41 +0530

    Refactored code structure for improved readability and maintainability

--- lib/view/sleep_screen/sleep_screen.dart
+++ lib/view/sleep_screen/sleep_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app_ui/utils/color_constant/color_constant.dart';
 
 class SleepScreen extends StatelessWidget {
   const SleepScreen({super.key});
@@ -7,7 +8,19 @@ class SleepScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/welcome sleep.png"),
+                fit: BoxFit.cover)),
+        child: Column(
+          children: [
+            Container(
+              height: 65,
+              width: 375,
+              decoration: BoxDecoration(color: txtBlueGrey),
+            )
+          ],
+        ),
       ),
     );
   }
