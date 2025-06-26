a83a662bd16732a5ec12c3dc2adfc1f459cbc3fe
AthiraaAthi <athiraa2612@gmail.com>
2025-06-26 17:28:44 +0530

    Removed unused Container and decoration from SleepScreen for cleaner code

--- lib/view/sleep_screen/sleep_screen.dart
+++ lib/view/sleep_screen/sleep_screen.dart
@@ -6,22 +6,6 @@ class SleepScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/welcome sleep.png"),
-                fit: BoxFit.cover)),
-        child: Column(
-          children: [
-            Container(
-              height: 65,
-              width: 375,
-              decoration: BoxDecoration(color: txtBlueGrey),
-            )
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
