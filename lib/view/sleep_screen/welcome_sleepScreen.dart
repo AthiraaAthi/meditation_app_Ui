bb456571157c912895c261d14476321cae908cfd
AthiraaAthi <athiraa2612@gmail.com>
2025-06-26 17:40:01 +0530

    Added WelcomeSleep widget with placeholder implementation

---
+++ lib/view/sleep_screen/welcome_sleepScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomeSleep extends StatelessWidget {
+  const WelcomeSleep({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
