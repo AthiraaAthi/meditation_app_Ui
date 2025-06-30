3a1bcead6bd815e643961bd2cd181256354b77ee
AthiraaAthi <athiraa2612@gmail.com>
2025-06-30 18:05:51 +0530

    Added spacing between ListView and bottom elements in SleepScreen

--- lib/view/sleep_screen/sleep_screen.dart
+++ lib/view/sleep_screen/sleep_screen.dart
@@ -69,6 +69,9 @@ class _SleepScreenState extends State<SleepScreen> {
                   ),
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
